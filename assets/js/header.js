a57e5c6414d3dc72afb416171c2fb0a3b010ac0a
Chu Đình Hiển <hienc1212@gmail.com>
2024-10-01 00:00:01 +0700

    fix header

---
+++ assets/js/header.js
@@ -0,0 +1,64 @@
+fetch('../../src/layouts/header.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('header').innerHTML = data;
+    executeHeaderScripts();
+    addLanguageSwitchEvents();
+  })
+  .catch(error => console.error('Error loading header:', error));
+
+const addLanguageSwitchEvents = () => {
+  const langViBtn = document.getElementById('lang-vi');
+  const langEnBtn = document.getElementById('lang-en');
+
+  if (langViBtn && langEnBtn) {
+    langViBtn.addEventListener('click', () => switchLanguage('vi', langViBtn, langEnBtn));
+    langEnBtn.addEventListener('click', () => switchLanguage('en', langEnBtn, langViBtn));
+  } else {
+    console.error('Không tìm thấy phần tử với id lang-vi hoặc lang-en');
+  }
+}
+
+const switchLanguage = (lang, activeBtn, inactiveBtn) => {
+  loadTranslations(lang);
+  activeBtn.classList.add('text-green-500');
+  inactiveBtn.classList.remove('text-green-500');
+}
+
+executeHeaderScripts = () =>{
+  const toggleMenu = () => document.getElementById('mobile-menu').classList.toggle('hidden');
+
+  document.getElementById('mobile-menu-toggle').addEventListener('click', toggleMenu);
+
+  const toggleVisibility = (elementId, show) => {
+    document.getElementById(elementId).classList.toggle('hidden', !show);
+  };
+
+  document.getElementById('mobile-icon-down').addEventListener('click', () => {
+    toggleVisibility('mobile-child-product', true);
+    toggleVisibility('mobile-icon-down', false);
+    toggleVisibility('mobile-icon-up', true);
+  });
+
+  document.getElementById('mobile-icon-up').addEventListener('click', () => {
+    toggleVisibility('mobile-child-product', false);
+    toggleVisibility('mobile-icon-down', true);
+    toggleVisibility('mobile-icon-up', false);
+  });
+
+  const searchForm = document.getElementById('search-form');
+  const searchInput = document.getElementById('search-input');
+
+  document.getElementById('search-icon').addEventListener('click', () => {
+    const isHidden = searchForm.classList.contains('hidden');
+    searchForm.classList.toggle('hidden', !isHidden);
+    if (isHidden) searchInput.focus();
+  });
+
+  searchInput.addEventListener('keydown', event => {
+    if (event.key === 'Enter' && searchInput.value.trim() !== '') {
+      event.preventDefault();
+      searchForm.submit();
+    }
+  });
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}", "./pages/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main:'#3fb871',
+        active: '#36a66d',
+        vimeo: '#1AB7EA',
+        tumblr: '#36465D'
+      },
+      container: {
+        center: true,
+        padding: '1rem',
+        screens: {
+          sm: '540px',
+          md: '720px',
+          lg: '960px',
+          xl: '1140px',
+          '2xl': '1140px',
+        },
+      },
+    },
   },
   plugins: [],
 }
