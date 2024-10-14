4cef52bc706580e19eb05abc823944d7ff305ef8
betosan2810 <vle80444@gmail.com>
2024-10-14 21:37:43 +0700

    man chi tiet tin tuc

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,4 +1,4 @@
-fetch('../../src/layouts/header.html')
+fetch('/src/layouts/header.html')
   .then(response => response.text())
   .then(data => {
     document.getElementById('header').innerHTML = data;
